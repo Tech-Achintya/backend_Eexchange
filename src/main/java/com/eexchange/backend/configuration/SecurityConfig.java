dc5514dda17ec8a6ea9d697a81bc0ae9437c2ac6
Achintya <achintya26022006@gmail.com>
2026-04-24 17:54:10 +0530

    Added

--- src/main/java/com/eexchange/backend/configuration/SecurityConfig.java
+++ src/main/java/com/eexchange/backend/configuration/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173","https://your-app.netlify.app"));
+        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173","https://eexchange-site.netlify.app/"));
         configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(java.util.List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
